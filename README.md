6dac56ac849abb1c3fba7ad4216943fdeff44907
axetroy <troy450409405@gmail.com>
2018-01-05 12:00:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+## RFC Date
+
+[![Build Status](https://travis-ci.org/axetroy/RFCDate.svg?branch=master)](https://travis-ci.org/axetroy/RFCDate)
+![License](https://img.shields.io/badge/license-Apache-green.svg)
+
+Format/Parse common standard date format.
+
+## Usage
+
+```javascript
+const { Now, Parse, Time } = require('rfcdate');
+console.log(time.Now().format(Time.RFC3339));
+console.log(new Time().format(Time.RFC3339Nano));
+```
+
+## Contributing
+
+[Contributing Guide](https://github.com/axetroy/RFCDate/blob/master/CONTRIBUTING.md)
+
+如果你觉得项目不错，不要吝啬你的 star.
+
+长期造轮子，欢迎 follow.
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+
+| [<img src="https://avatars1.githubusercontent.com/u/9758711?v=3" width="100px;"/><br /><sub>Axetroy</sub>](http://axetroy.github.io)<br />[💻](https://github.com/axetroy/RFCDate/commits?author=axetroy) [🐛](https://github.com/axetroy/RFCDate/issues?q=author%3Aaxetroy) 🎨 |
+| :-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------: |
+
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## License
+
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Faxetroy%2FRFCDate.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2Faxetroy%2FRFCDate?ref=badge_large)
---
+++ index.js
@@ -0,0 +1,106 @@
+const moment = require('moment');
+
+// default utc offset
+const defaultUtcOffset = moment().utcOffset();
+
+/**
+ * Get Now Date
+ * @returns {Time}
+ * @constructor
+ */
+function Now() {
+  return new Time(new Date());
+}
+
+/**
+ * Parse Date from a string any layer
+ * @param dateString
+ * @param layer
+ * @returns {Date}
+ * @constructor
+ */
+function Parse(dateString, layer) {
+  const utcOffset = layer === Time.ISO8601Z ? 0 : defaultUtcOffset;
+  return moment(dateString, layer)
+    .utcOffset(utcOffset)
+    .toDate();
+}
+
+/**
+ * Time constructor
+ * @param d
+ * @constructor
+ */
+function Time(d) {
+  this.date = new Date(d);
+  Object.defineProperty(this, 'date', {
+    enumerable: false,
+    configurable: false,
+    value: new Date(d)
+  });
+}
+
+Time.prototype.constructor = Time;
+
+/**
+ * rewrite to string
+ * @returns {string}
+ */
+Time.prototype.toString = function() {
+  return this.date.toString();
+};
+
+/**
+ * rewrite valueOf
+ * @returns {*}
+ */
+Time.prototype.valueOf = function() {
+  return this.date.valueOf();
+};
+
+// time format layer
+Time.ANSIC = 'ddd MMM D  HH:mm:ss YYYY';
+Time.UnixDate = 'ddd MMM  D HH:mm:ss z YYYY';
+Time.RubyDate = 'ddd MMM D HH:mm:ss ZZ YYYY';
+Time.RFC822 = 'ddd MMM D HH:mm:ss ZZ YYYY z';
+Time.RFC822Z = 'D MMM YY HH:mm ZZ';
+Time.RFC850 = 'dddd, DD-MMM-YY HH:mm:ss z';
+Time.RFC1123 = 'ddd, DD MMM YY HH:mm:ss z';
+Time.RFC3339 = 'YYYY-MM-DDTHH:mm:SSZ';
+Time.RFC3339Nano = 'YYYY-MM-DDTHH:mm.SSSSSS:SSZ';
+Time.Kitchen = 'h:mA';
+Time.Stamp = 'MMM  D HH:mm:ss';
+Time.StampMilli = 'MMM  D HH:mm:ss';
+Time.StampMicro = 'MMM  D HH:mm:ss.SSSSSS';
+Time.StampNano = 'MMM  D HH:mm:ss.SSSSSSSSS';
+Time.ISO8601 = 'YYYY-MM-DDTHH:mm:ss ZZ';
+Time.ISO8601Z = 'YYYY-MM-DDTHH:mm:ss\\Z';
+Time.ISODate = 'YYYY-MM-DD';
+Time.ISOOrigin = 'YYYY-DDD';
+Time.ISOTime = 'HH:mm:ss';
+Time.ISOWeek = 'YYYY-\\Www';
+Time.ISOWeekDay = 'YYYY-\\Www-d';
+Time.SlashedDate = 'MM/DD/YYYY';
+Time.StrfTimeClock = 'HH:mm';
+Time.StrfTimeISODate = 'YYYY-MM-DD';
+Time.StrfTimeKitchen = 'H:MM:SS A';
+Time.StrfTimeShortdate = 'D-MMM-YYYY';
+Time.UTC = 'ddd, DD MMM YYYY HH:mm:ss z';
+
+/**
+ * format the date with layer
+ * @param layer
+ * @returns {string}
+ */
+Time.prototype.format = function(layer) {
+  const utcOffset = layer === Time.ISO8601Z ? 0 : defaultUtcOffset;
+  return moment(this.date)
+    .utcOffset(utcOffset)
+    .format(layer);
+};
+
+module.exports = {
+  Now: Now,
+  Parse: Parse,
+  Time: Time
+};
